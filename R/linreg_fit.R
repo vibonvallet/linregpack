4f93fe984ec885ff2d9bef34cc505a149c19a0a7
vibonvallet <vibon@umich.edu>
2025-11-19 09:28:42 -0500

    Update linreg_fit.R

--- R/linreg_fit.R
+++ R/linreg_fit.R
@@ -1,3 +1,8 @@
+#' Fit a linear regression
+#'
+#' @param formula A formula object
+#' @param data A data frame
+#' @return A linregpack object
 #' @export
 linreg_fit <- function(formula, data) {
   mf <- model.frame(formula, data=data)
@@ -12,7 +17,7 @@ linreg_fit <- function(formula, data) {
   vc = sigma2 * chol2inv(qr.R(qrX))
   se = sqrt(diag(vc))
   
-  # Minimal addition: store formula
+  # Minimal addition: store formula for predict
   out <- list(
     coefficients = coef,
     fitted.values = fitted,
@@ -24,7 +29,7 @@ linreg_fit <- function(formula, data) {
     call = match.call(),
     terms = attr(mf, "terms"),
     model = mf,
-    formula = formula      # <-- added
+    formula = formula
   )
   
   class(out) <- "linregpack"
