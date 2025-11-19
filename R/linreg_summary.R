02078ece457ec278f6a70c4dd2904d843cc7f0fa
vibonvallet <vibon@umich.edu>
2025-11-19 09:04:28 -0500

    Update linreg_summary.R

--- R/linreg_summary.R
+++ R/linreg_summary.R
@@ -1,10 +1,24 @@
 #' @export
-print.linregpack <- function(x, ...) {
-  print(x$call)
-  print(cbind(Estimate=x$coefficients, SE=x$se))
+summary.linregpack <- function(object, ...) {
+  cat("Call:\n")
+  print(object$call)
+  
+  # Coefficients table
+  coef_table <- cbind(
+    Estimate = object$coefficients,
+    Std.Error = object$se
+  )
+  cat("\nCoefficients:\n")
+  print(coef_table)
+  
+  # Residuals summary
+  cat("\nResiduals:\n")
+  print(summary(object$y - fitted(object)))
+  
+  invisible(object)
 }
 
 #' @export
-summary.linregpack <- function(object, ...) {
-  print(object)
+linreg_summary <- function(fit) {
+  summary(fit)
 }
