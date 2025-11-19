a71bc714cc249b626e4d68da9891a1281c5cd67a
vibonvallet <vibon@umich.edu>
2025-11-19 09:29:09 -0500

    Update linreg_predict.R

--- R/linreg_predict.R
+++ R/linreg_predict.R
@@ -1,10 +1,20 @@
+#' Predict method for linregpack objects
+#'
+#' @param object A linregpack object
+#' @param newdata Optional new data frame
+#' @param ... Additional arguments (ignored)
 #' @export
 predict.linregpack <- function(object, newdata=NULL, ...) {
   if (is.null(newdata)) return(object$fitted.values)
-  Xnew <- model.matrix(object$terms, data=newdata)
+  Xnew <- model.matrix(object$formula, data=newdata)
   as.vector(Xnew %*% object$coefficients)
 }
 
+#' Wrapper function for prediction
+#'
+#' @param fit A linregpack object
+#' @param newdata Optional new data frame
+#' @param ... Additional arguments (ignored)
 #' @export
 linreg_predict <- function(fit, newdata = NULL, ...) {
   predict(fit, newdata = newdata, ...)
